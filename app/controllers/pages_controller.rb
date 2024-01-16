69b0a9e32c871d582055099dea4bb5944f150425
luyeewong <wongluyee1015@hotmail.com>
2024-01-16 11:27:59 +0900

    add dotenv and simple form

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,18 @@
+require 'uri'
+require 'net/http'
+
 class PagesController < ApplicationController
   def search
+    url = URI("https://api.listingleopard.com/single/search-page?domain=amazon.co.jp&search_term=eloquent%20ruby&category_id=2250738051")
+
+    http = Net::HTTP.new(url.host, url.port)
+    http.use_ssl = true
+
+    request = Net::HTTP::Get.new(url)
+    request["accept"] = 'application/json'
+    request["X-api-key"] = "#{ENV['LEOPARD_LISTING_API_KEY']}"
+
+    response = http.request(request)
+    puts response.read_body
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,7 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   # root "articles#index"
   root 'pages#search'
+
+  # search_path (simple form)
+  get 'search', to: 'pages#search'
 end
